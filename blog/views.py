21d0657e835f08996b060d6968c5ffa927eca8c7
Pavlo Pavlo <pavlo.kravets.kb.2022@lpnu.ua>
2024-03-19 09:27:56 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import redirect
 from blog.forms import CommentForm
 from blog.models import Post
 
+import logging 
+
+logger = logging.getLogger(__name__)
+
 def index(request):
+
   posts = Post.objects.all()
-  print(posts)
+  logger.debug('Got %d posts', len(posts))
+
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
@@ -20,6 +26,9 @@ def post_detail(request, slug):
         comment.content_object = post
         comment.creator = request.user
         comment.save()
+        logger.info(
+          'Created comment on Post %d for user %s', post.pk, request.user
+        )
         return redirect(request.path_info)
     else:
       comment_form = CommentForm()
